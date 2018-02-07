a3be144430b22bdccd77577968b85e688be5adbf
Agent57 <lee@agent57.co.uk>
2018-02-07 01:09:32 +0000

    Added stored create/read procedures for an individual contractor id;
    - contractor record (joins contractor/passport/emergency contact tables)
    - contractor history
    - contractor notes

--- Database/DatabaseInstall.sql
+++ Database/DatabaseInstall.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Feb 06, 2018 at 10:57 PM
+-- Generation Time: Feb 07, 2018 at 12:55 AM
 -- Server version: 5.7.19
 -- PHP Version: 5.6.31
 
@@ -18,6 +18,141 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `fireaid_personnel` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `fireaid_personnel`;
 
+DELIMITER $$
+--
+-- Procedures
+--
+DROP PROCEDURE IF EXISTS `create_contractor_history`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_contractor_history` (IN `idProject` INT(11), IN `idContractor` INT(11))  BEGIN
+	INSERT INTO `history`
+	(
+		`fkProject`,
+		`fkContractor`
+	)
+	VALUES
+	(
+		idProject,
+		idContractor
+	);
+
+END$$
+
+DROP PROCEDURE IF EXISTS `create_contractor_note`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_contractor_note` (IN `text` VARCHAR(200), IN `idContractor` INT(11), IN `idUser` INT(11))  BEGIN
+	INSERT INTO `note`
+	(
+		`Text`,
+		`fkContractor`,
+		`fkUser`
+	)
+	VALUES
+	(
+		text,
+		idContractor,
+		idUser
+	);
+END$$
+
+DROP PROCEDURE IF EXISTS `create_contractor_record`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_contractor_record` (IN `title` VARCHAR(45), IN `givenNames` VARCHAR(45), IN `surname` VARCHAR(45), IN `phoneNumber` VARCHAR(45), IN `gender` VARCHAR(45), IN `dateOfBirth` DATETIME, IN `issueDate` DATETIME, IN `expiryDate` DATETIME, IN `nationality` VARCHAR(45), IN `imageUrl` VARCHAR(45), IN `name` VARCHAR(45), IN `relationship` VARCHAR(45), IN `contactNumber` VARCHAR(45))  BEGIN
+	DECLARE idContractor INT(11);
+	DECLARE msg TEXT;
+    DECLARE code CHAR(5) DEFAULT '00000';
+    
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		GET DIAGNOSTICS CONDITION 1
+		code = RETURNED_SQLSTATE, msg = MESSAGE_TEXT;
+		ROLLBACK;
+	END;
+	
+    START TRANSACTION;
+    
+    INSERT INTO contractor
+    (
+		`Title`,
+		`GivenNames`,
+		`Surname`,
+		`PhoneNumber`,
+		`Gender`,
+		`DateOfBirth`
+	)
+	VALUES
+	(
+		title,
+		givenNames,
+		surname,
+		phoneNumber,
+		gender,
+		dateOfBirth
+	);
+
+
+	SET idContractor = (SELECT LAST_INSERT_ID());
+    
+	INSERT INTO `passport`
+	(
+		`Number`,
+		`IssueDate`,
+		`ExpiryDate`,
+		`Nationality`,
+		`ImageUrl`,
+		`fkContractor`
+	)
+	VALUES
+	(
+		number,
+		issueDate,
+		expiryDate,
+		nationality,
+		imageUrl,
+		idContractor
+	);
+
+	INSERT INTO `emergency_contact`
+	(
+		`Name`,
+		`Relationship`,
+		`PhoneNumber`,
+		`fkContractor`
+	)
+	VALUES
+	(
+		name,
+		relationship,
+		contactNumber,
+		idContractor
+	);
+
+	COMMIT;
+    SELECT idContractor;
+END$$
+
+DROP PROCEDURE IF EXISTS `read_contractor_history`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `read_contractor_history` (IN `idContractor` INT)  BEGIN
+	select project.idProject, project.Location, project.Description, project.Date
+    from project
+    inner join history on project.idProject = history.fkProject
+    where history.fkContractor = idContractor;
+END$$
+
+DROP PROCEDURE IF EXISTS `read_contractor_notes`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `read_contractor_notes` (IN `idContractor` INT)  BEGIN
+	select idNote, Text, fkUser from note
+    where fkContractor = idContractor;
+END$$
+
+DROP PROCEDURE IF EXISTS `read_contractor_record`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `read_contractor_record` (IN `idContractor` INT)  BEGIN
+	select contractor.*, passport.*, emergency_contact.*
+    from contractor
+    join passport on contractor.idContractor = passport.fkContractor
+    join emergency_contact on contractor.idContractor = emergency_contact.fkContractor
+    where contractor.idContractor = idContractor;
+END$$
+
+DELIMITER ;
+
 -- --------------------------------------------------------
 
 --
@@ -25,16 +160,15 @@ USE `fireaid_personnel`;
 --
 
 DROP TABLE IF EXISTS `contractor`;
-CREATE TABLE IF NOT EXISTS `contractor` (
-  `idContractor` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `contractor` (
+  `idContractor` int(11) NOT NULL,
   `Title` varchar(45) NOT NULL,
   `GivenNames` varchar(45) NOT NULL,
   `Surname` varchar(45) NOT NULL,
   `PhoneNumber` varchar(45) NOT NULL,
   `Gender` varchar(45) NOT NULL,
-  `DateOfBirth` datetime NOT NULL,
-  PRIMARY KEY (`idContractor`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `DateOfBirth` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -43,15 +177,13 @@ CREATE TABLE IF NOT EXISTS `contractor` (
 --
 
 DROP TABLE IF EXISTS `emergency_contact`;
-CREATE TABLE IF NOT EXISTS `emergency_contact` (
-  `idEmergencyContact` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `emergency_contact` (
+  `idEmergencyContact` int(11) NOT NULL,
   `Name` varchar(45) NOT NULL,
   `Relationship` varchar(45) NOT NULL,
   `PhoneNumber` varchar(45) NOT NULL,
-  `fkContractor` int(11) NOT NULL,
-  PRIMARY KEY (`idEmergencyContact`),
-  KEY `fkPersonal_idx` (`fkContractor`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `fkContractor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -60,14 +192,11 @@ CREATE TABLE IF NOT EXISTS `emergency_contact` (
 --
 
 DROP TABLE IF EXISTS `history`;
-CREATE TABLE IF NOT EXISTS `history` (
-  `idHistory` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `history` (
+  `idHistory` int(11) NOT NULL,
   `fkProject` int(11) NOT NULL,
-  `fkContractor` int(11) NOT NULL,
-  PRIMARY KEY (`idHistory`),
-  KEY `FK__history__personal_idx` (`fkContractor`),
-  KEY `FK__history__project_idx` (`fkProject`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `fkContractor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -76,15 +205,12 @@ CREATE TABLE IF NOT EXISTS `history` (
 --
 
 DROP TABLE IF EXISTS `note`;
-CREATE TABLE IF NOT EXISTS `note` (
-  `idNote` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `note` (
+  `idNote` int(11) NOT NULL,
   `Text` varchar(200) NOT NULL,
   `fkContractor` int(11) NOT NULL,
-  `fkUser` int(11) NOT NULL,
-  PRIMARY KEY (`idNote`),
-  KEY `FK__notes__personal_idx` (`fkContractor`),
-  KEY `FK__notes__user_idx` (`fkUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `fkUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -93,17 +219,15 @@ CREATE TABLE IF NOT EXISTS `note` (
 --
 
 DROP TABLE IF EXISTS `passport`;
-CREATE TABLE IF NOT EXISTS `passport` (
-  `idPassport` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `passport` (
+  `idPassport` int(11) NOT NULL,
   `Number` varchar(45) NOT NULL,
   `IssueDate` datetime NOT NULL,
   `ExpiryDate` datetime NOT NULL,
   `Nationality` varchar(45) NOT NULL,
   `ImageUrl` varchar(45) NOT NULL,
-  `fkContractor` int(11) NOT NULL,
-  PRIMARY KEY (`idPassport`),
-  KEY `fkPersonal_idx` (`fkContractor`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `fkContractor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -112,13 +236,12 @@ CREATE TABLE IF NOT EXISTS `passport` (
 --
 
 DROP TABLE IF EXISTS `project`;
-CREATE TABLE IF NOT EXISTS `project` (
-  `idProject` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `project` (
+  `idProject` int(11) NOT NULL,
   `Location` varchar(45) NOT NULL,
   `Description` varchar(45) NOT NULL,
-  `Date` datetime NOT NULL,
-  PRIMARY KEY (`idProject`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `Date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -127,13 +250,114 @@ CREATE TABLE IF NOT EXISTS `project` (
 --
 
 DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `idUser` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `idUser` int(11) NOT NULL,
   `Name` varchar(45) NOT NULL,
+  `Email` varchar(45) NOT NULL,
   `Login` varchar(45) NOT NULL,
   `Password` varchar(45) NOT NULL,
-  PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `Enabled` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contractor`
+--
+ALTER TABLE `contractor`
+  ADD PRIMARY KEY (`idContractor`);
+
+--
+-- Indexes for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  ADD PRIMARY KEY (`idEmergencyContact`),
+  ADD KEY `fkPersonal_idx` (`fkContractor`);
+
+--
+-- Indexes for table `history`
+--
+ALTER TABLE `history`
+  ADD PRIMARY KEY (`idHistory`),
+  ADD KEY `FK__history__personal_idx` (`fkContractor`),
+  ADD KEY `FK__history__project_idx` (`fkProject`);
+
+--
+-- Indexes for table `note`
+--
+ALTER TABLE `note`
+  ADD PRIMARY KEY (`idNote`),
+  ADD KEY `FK__notes__personal_idx` (`fkContractor`),
+  ADD KEY `FK__notes__user_idx` (`fkUser`);
+
+--
+-- Indexes for table `passport`
+--
+ALTER TABLE `passport`
+  ADD PRIMARY KEY (`idPassport`),
+  ADD KEY `fkPersonal_idx` (`fkContractor`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`idProject`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `Login` (`Login`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contractor`
+--
+ALTER TABLE `contractor`
+  MODIFY `idContractor` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `emergency_contact`
+--
+ALTER TABLE `emergency_contact`
+  MODIFY `idEmergencyContact` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `history`
+--
+ALTER TABLE `history`
+  MODIFY `idHistory` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `note`
+--
+ALTER TABLE `note`
+  MODIFY `idNote` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `passport`
+--
+ALTER TABLE `passport`
+  MODIFY `idPassport` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `idProject` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -150,14 +374,14 @@ ALTER TABLE `emergency_contact`
 --
 ALTER TABLE `history`
   ADD CONSTRAINT `FK__history__contractor` FOREIGN KEY (`fkContractor`) REFERENCES `contractor` (`idContractor`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK__history__project` FOREIGN KEY (`fkProject`) REFERENCES `project` (`idProject`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK__history__project` FOREIGN KEY (`fkProject`) REFERENCES `project` (`idProject`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `note`
 --
 ALTER TABLE `note`
   ADD CONSTRAINT `FK__notes__contractor` FOREIGN KEY (`fkContractor`) REFERENCES `contractor` (`idContractor`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK__notes__user` FOREIGN KEY (`fkUser`) REFERENCES `user` (`idUser`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK__notes__user` FOREIGN KEY (`fkUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `passport`
